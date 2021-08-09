e5548d239ed36007c564c45d6e40aad7f235a0a4
Stephen Rodrigues Knupfer <srodrigues@springglobal.com>
2021-08-09 20:37:44 -0300

    resolucao do desafio

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -30,15 +30,23 @@ export function Home() {
 
   async function loadData() {
     const dataKey = '@savepass:logins';
-    // Get asyncStorage data, use setSearchListData and setData
+    const data = await AsyncStorage.getItem(dataKey);
+    const parsedData = data ? JSON.parse(data) : [];
+    setData(parsedData);
+    setSearchListData(parsedData);
   }
 
   function handleFilterLoginData() {
-    // Filter results inside data, save with setSearchListData
+    if (searchText.trim() !== '') {
+      const filteredData = searchListData.filter(data => data.service_name.includes(searchText.trim()));
+      setSearchListData(filteredData);
+    } else {
+      setSearchListData(data);
+    }
   }
 
   function handleChangeInputText(text: string) {
-    // Update searchText value
+    setSearchText(text);
   }
 
   useFocusEffect(useCallback(() => {
